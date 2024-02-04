58dc4ded3d872d872aa320d03c2249526c5f7070
Alexis (Poliorcetics) Bourget <ab_github@poliorcetiq.eu>
2024-02-04 19:18:09 +0100

    add bat config

---
+++ bat.nix
@@ -0,0 +1,13 @@
+# Bat configuration
+#
+# <https://github.com/sharkdp/bat>
+
+{ config, ... }:
+{
+  programs.bat.enable = true;
+
+  # Settings: <https://github.com/sharkdp/bat#configuration-file>
+  programs.bat.config = {
+    theme = "Coldark-Dark";
+  };
+}
