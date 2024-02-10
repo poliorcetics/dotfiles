b1e0b4cdc823036996488e89681e3b8a300d534a
Alexis (Poliorcetics) Bourget <ab_github@poliorcetiq.eu>
2024-02-10 02:40:54 +0100

    home.activation: move to a separate file for clarity

--- home.nix
+++ home.nix
@@ -97,9 +97,6 @@ let
     yaml-language-server # LSP for YAML
   ];
 
-  # Import my helper functions
-  funcs = import ./functions.nix { inherit config lib pkgs; };
-
   in
 
   {
@@ -117,31 +114,7 @@ let
     home.homeDirectory = "/Users/alexis";
 
     # Scripts to run on `home-manager switch`
-    home.activation = {
-      # Bacon doesn't respect XDG spec on macOS, force it to
-      linkBaconConfig = funcs.createAppSupportSymlink {
-        xdg_subdir = "bacon";
-        app_support_link = "org.dystroy.bacon";
-      };
-      # Same for Nushell
-      linkNushellConfig = funcs.createAppSupportSymlink {
-        xdg_subdir = "nushell";
-        app_support_link = "nushell";
-      };
-      # Same for Pijul
-      linkPijulConfig = funcs.createAppSupportSymlink {
-        xdg_subdir = "pijul";
-        app_support_link = "pijul";
-      };
-
-      # Create my expected directories
-      makeDirs = ''
-        run mkdir -p ~/repos/me/
-        run mkdir -p ~/repos/tp/
-        run mkdir -p ~/repos/work/priv/
-        run mkdir -p ~/repos/work/pub/
-      '';
-    };
+    home.activation = import ./home-manager/activation.nix { inherit config lib pkgs; };
 
     # XDG setup
     xdg.enable = true;
