176b9152991112e2945b10dafef19d0c3238a7b7
Alexis (Poliorcetics) Bourget <ab_contribs@poliorcetiq.eu>
2024-08-18 19:26:12 +0200

    Back out "rust-analyzer: force locked install while there is a bug"
    This backs out commit c697f1fe9984fb8bee71f9bb9fb06ae815d844fa.

--- home/topgrade.nix
+++ home/topgrade.nix
@@ -7,7 +7,6 @@
 let
 
   helixRepo = "${config.xdg.cacheHome}/helix-repo";
-  ra-target-dir = "${config.xdg.cacheHome}/target-dirs/cargo/rust-analyzer-topgrade";
 
 in
 {
@@ -17,8 +16,6 @@ in
   programs.topgrade.settings = {
     commands = {
       "1. Helix - Install from repo" = "cd ${helixRepo} && cargo +stable install --locked --path helix-term && hx --grammar fetch && hx --grammar build";
-      # Locked install because of https://github.com/rust-lang/rust-analyzer/issues/17914 for now
-      "2. Rust-Analyzer - Install from git" = "CARGO_TARGET_DIR=${ra-target-dir} cargo +stable install --locked --git https://github.com/rust-lang/rust-analyzer --branch master --force rust-analyzer";
     };
 
     firmware.upgrade = false;
