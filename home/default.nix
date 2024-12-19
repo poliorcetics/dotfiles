e0debc28fb29afb39eec991e733e4a9a2fa6b47b
Alexis (Poliorcetics) Bourget <ab_contribs@poliorcetiq.eu>
2024-12-19 22:50:46 +0100

    hm: remove default docs

--- home/default.nix
+++ home/default.nix
@@ -125,7 +125,6 @@ in
     "/opt/homebrew/bin"
   ];
 
-  # The home.packages option allows you to install Nix packages into your environment.
   home.packages =
     cargoPackages
     ++ nodePackages
@@ -133,17 +132,6 @@ in
     ++ rustPackages
     ++ miscPackages
     ++ [
-      # It is sometimes useful to fine-tune packages, for example, by applying overrides. You can do
-      # that directly here, just don't forget the parentheses. Maybe you want to install Nerd Fonts
-      # with a limited number of fonts?
-      #: (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-      # You can also create simple shell scripts directly inside your configuration. For example,
-      # this adds a command 'my-hello' to your environment:
-      #: (pkgs.writeShellScriptBin "my-hello" ''
-      #:   echo "Hello, ${config.home.username}!"
-      #: '')
-
       (pkgs.writeScriptBin "hn" (builtins.readFile ./scripts/hn.nu))
       (pkgs.writeScriptBin "rust-analyzer-wrapper" (builtins.readFile ./scripts/rust-analyzer-wrapper.nu))
       (pkgs.writeScriptBin "systemfiles" (builtins.readFile ./scripts/systemfiles.nu))
@@ -161,23 +149,6 @@ in
       )
     ];
 
-  # Home Manager can also manage your environment variables through 'home.sessionVariables'. If
-  # you don't want to manage your shell through Home Manager then you have to manually source
-  # 'hm-session-vars.sh' located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/alexis/etc/profile.d/hm-session-vars.sh
-  #
-  # Those variables are only defined once, opening a new shell does not redefine them I think.
-  #
-  # The `imports` list above can also define additional members.
   home.sessionVariables = {
     EDITOR = "hx";
     VISUAL = "hx";
