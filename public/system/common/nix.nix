6ffd80cc4e14f3df05063a9e9e50888cc908fccb
Alexis (Poliorcetics) Bourget <ab_contribs@poliorcetiq.eu>
2025-02-12 23:08:06 +0100

    nix: alphanumerical order for some options

--- public/system/common/nix.nix
+++ public/system/common/nix.nix
@@ -32,11 +32,11 @@
   # <https://github.com/NixOS/nixpkgs/blob/22c3f2cf41a0e70184334a958e6b124fb0ce3e01/nixos/modules/config/nix.nix>
   # <https://github.com/LnL7/nix-darwin/blob/a35b08d09efda83625bef267eb24347b446c80b8/modules/nix/default.nix>
   nix.settings = {
-    # Necessary for using flakes on this system.
-    experimental-features = "nix-command flakes";
-    trusted-users = [ userDetails.username ];
     # Technically a GC config but not under nix.gc.
     # Disabled after <https://github.com/NixOS/nix/issues/7273>.
     auto-optimise-store = false;
+    # Necessary for using flakes on this system.
+    experimental-features = "nix-command flakes";
+    trusted-users = [ userDetails.username ];
   };
 }
