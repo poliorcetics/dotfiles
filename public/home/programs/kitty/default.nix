4a4404149224075a2c03c0789987c19de1f19c70
Alexis (Poliorcetics) Bourget <ab_contribs@poliorcetiq.eu>
2025-02-19 10:05:50 +0100

    kitty: use no-rc and not disabled for shell integration

--- public/home/programs/kitty/default.nix
+++ public/home/programs/kitty/default.nix
@@ -19,8 +19,6 @@
 
   # On macOS I use `brew` to manage Kitty
   programs.kitty.enable = pkgs.stdenv.isLinux;
-  # We do manual shell integration
-  programs.kitty.shellIntegration.mode = "disabled";
 
   xdg.configFile."kitty/includes/editor.conf".text = ''
     # <https://sw.kovidgoyal.net/kitty/conf/#opt-kitty.editor>
