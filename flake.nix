76b7f50f2408a94fa35214c236a99ed863ec62b4
Alexis (Poliorcetics) Bourget <ab_contribs@poliorcetiq.eu>
2026-01-09 19:35:32 -0300

    flake: prepare nicer integration of machines

--- flake.nix
+++ flake.nix
@@ -32,31 +32,38 @@
       treefmt,
       ...
     }@inputs:
-    {
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#mac
-      darwinConfigurations.mac = import ./. inputs "aarch64-darwin";
-      checks.aarch64-darwin.macos-darwin-system = self.darwinConfigurations.mac.system;
+    let
+      recursiveMerge = nixpkgs.lib.foldr nixpkgs.lib.recursiveUpdate { };
+    in
+    recursiveMerge [
+      {
+        # Build darwin flake using:
+        # $ darwin-rebuild build --flake .#mac
+        darwinConfigurations.mac = import ./. inputs "aarch64-darwin";
+        checks.aarch64-darwin.macos-darwin-system = self.darwinConfigurations.mac.system;
 
-      homeConfigurations.linux = import ./. inputs "aarch64-linux";
-      checks.aarch64-linux.linux-hm-system = self.homeConfigurations.linux.activationPackage;
+        homeConfigurations.linux = import ./. inputs "aarch64-linux";
+        checks.aarch64-linux.linux-hm-system = self.homeConfigurations.linux.activationPackage;
+      }
 
-      formatter =
-        let
-          # TODO: treefmt is in regular nixpkgs, look into using that
-          makeFmt = system: {
-            ${system} =
-              (treefmt.lib.evalModule (import nixpkgs { inherit system; }) {
-                programs.mdformat.enable = true;
-                programs.nixfmt.enable = true;
-                # NOTE: no `nufmt`, it's entirely broken
-                programs.ruff-format.enable = true;
-                programs.shfmt.enable = true;
-                programs.taplo.enable = true;
-                programs.yamlfmt.enable = true;
-              }).config.build.wrapper;
-          };
-        in
-        makeFmt "aarch64-darwin" // makeFmt "aarch64-linux" // makeFmt "x86_64-linux";
-    };
+      {
+        formatter =
+          let
+            # TODO: treefmt is in regular nixpkgs, look into using that
+            makeFmt = system: {
+              ${system} =
+                (treefmt.lib.evalModule (import nixpkgs { inherit system; }) {
+                  programs.mdformat.enable = true;
+                  programs.nixfmt.enable = true;
+                  # NOTE: no `nufmt`, it's entirely broken
+                  programs.ruff-format.enable = true;
+                  programs.shfmt.enable = true;
+                  programs.taplo.enable = true;
+                  programs.yamlfmt.enable = true;
+                }).config.build.wrapper;
+            };
+          in
+          makeFmt "aarch64-darwin" // makeFmt "aarch64-linux" // makeFmt "x86_64-linux";
+      }
+    ];
 }
