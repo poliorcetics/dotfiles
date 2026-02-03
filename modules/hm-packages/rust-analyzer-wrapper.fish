35a56c455725cf97011daaef93bea2764dac6bfa
Alexis (Poliorcetics) Bourget <ab_contribs@poliorcetiq.eu>
2026-02-03 15:16:17 +0100

    rust-analyzer: fix fish wrapper to not infinitely call itself

--- modules/hm-packages/rust-analyzer-wrapper.fish
+++ modules/hm-packages/rust-analyzer-wrapper.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 
-function rust-analyzer-wrapper --wraps rust-analyzer
+function rust-analyzer-wrapper-main --wraps rust-analyzer
     set --local workspace_root (cargo metadata --no-deps --format-version 1 --no-default-features --offline | jq .workspace_root | path normalize)
     set --local target_dir $XDG_CACHE_HOME/target-dirs/ra/(path basename $workspace_root)-(md5 -s $workspace_root)
 
     CARGO_TARGET_DIR=$target_dir exec rust-analyzer $argv
 end
 
-exec rust-analyzer-wrapper $argv
+rust-analyzer-wrapper-main $argv
